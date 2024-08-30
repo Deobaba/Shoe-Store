5e1282da56d3ded08a2707dc085f0c5dd6d4b3ad
Deobaba <adexsquare4192@gmail.com>
2024-08-30 05:11:19 +0100

    services and special offer

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
       '3xl': ['28px', '50px'],
       '4xl': ['48px', '58px'],
       '8xl': ['96px', '106px']
-    },
+    }, 
     extend: {
       fontFamily: {
         palanquin: ['Palanquin', 'sans-serif'],
